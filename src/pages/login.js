49dbaabf1ea36c25d7626fc8ce5e22b4e9c4f9a0
iswarya lakshmi <iswarya@fabric.inc>
2021-12-16 20:54:14 +0530

    installed-template, routes configuration

---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+    render() {
+        return (
+           <div>
+               <div>Login Form</div>
+               <div>Getting Started
+                This page is an overview of the React documentation and related resources.</div>
+           </div>
+        )
+    }
+}
+
+export default LoginForm
---
+++ src/pages/signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SignUpForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <div> Signup Form</div>
+            </div>
+        )
+    }
+}
+
+export default SignUpForm
